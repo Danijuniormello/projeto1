9f07d3b15bcd0d8ffb5285019c80d0de1a2238ea
Danijuniormello <141072103+Danijuniormello@users.noreply.github.com>
2023-09-04 16:12:39 -0400

    Update JavaVisita.js

--- JavaVisita.js
+++ JavaVisita.js
@@ -1,20 +1,29 @@
 let currentImageIndex = 0;
 const images = document.querySelectorAll('.image');
 
-function changeImage(direction) {
+function changeFullScreenImage(direction) {
   currentImageIndex += direction;
   if (currentImageIndex < 0) {
     currentImageIndex = images.length - 1;
   } else if (currentImageIndex >= images.length) {
     currentImageIndex = 0;
   }
-  showImage(currentImageIndex);
+  showFullScreenImage(currentImageIndex);
 }
 
-function showImage(index) {
-  images.forEach((image, i) => {
-    image.style.display = i === index ? 'block' : 'none';
-  });
+function openFullScreen(index) {
+  currentImageIndex = index;
+  showFullScreenImage(currentImageIndex);
+  document.querySelector('.fullscreen-container').style.display = 'block';
 }
 
-showImage(currentImageIndex);
+function closeFullScreen() {
+  document.querySelector('.fullscreen-container').style.display = 'none';
+}
+
+function showFullScreenImage(index) {
+  const fullscreenImage = document.querySelector('.fullscreen-image');
+  fullscreenImage.src = images[index].src;
+}
+
+showFullScreenImage(currentImageIndex);
